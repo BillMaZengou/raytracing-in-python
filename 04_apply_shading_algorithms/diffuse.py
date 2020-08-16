bb1bf14450a2d5787bbb6a9778f758c2aca6c9ad
Bill Ma <mazengou@gmail.com>
2020-08-16 21:40:26 +0800

    Add material and light source. Hence, diffusion is done

---
+++ 04_apply_shading_algorithms/diffuse.py
@@ -0,0 +1,93 @@
+import sys, os
+LIB_PATH = '../01_point_in_3d_space'
+sys.path.append(LIB_PATH)
+from Vector import *
+from mathTools import sqrt
+LIB_PATH = '../02_revealing_the_true_colors'
+sys.path.append(LIB_PATH)
+from color import *
+LIB_PATH = '../03_3d_balls_in_2d_space'
+sys.path.append(LIB_PATH)
+from simplified_raytracing import *
+
+class Material(object):
+    """docstring for Material."""
+
+    def __init__(self, diffusion, specular):
+        super(Material, self).__init__()
+        self.diffusion = diffusion
+        self.specular = specular
+
+class Light(object):
+    """docstring for Light."""
+
+    def __init__(self, source, direction):
+        super(Light, self).__init__()
+        self.source = source
+        self.direction = direction
+
+class Ball(object):
+    """docstring for Ball."""
+    def __init__(self, origin, radius, col, material):
+        super(Ball, self).__init__()
+        self.origin = origin
+        self.radius = radius
+        self.col = col
+        self.material = material
+
+    def getColor(self, hit_point, light_direction):
+        normal_vec = hit_point - self.origin
+        normal_vec /= normal_vec.get_norm()
+        color_vec = abs(light_direction.dot(normal_vec))*self.material.diffusion*self.col
+        return color_vec
+
+def main():
+    WIDTH = 320
+    HEIGHT = 200
+
+    AspectRatio = aspect(WIDTH, HEIGHT)
+    xMax = 1
+    xMin = -xMax
+    yMax = xMax/AspectRatio
+    yMin = -yMax
+
+    camera_pos = vec3d(0, 0, -1)
+    ball_pos = vec3d(0, 0, 0)
+    ball_r = 0.5
+    ball_material = Material(0.9, 0.1)
+
+    point_light = Light(vec3d(5, 5, -1/2), vec3d(-1, -1, 1).get_unit_vec())
+
+    red = color(255, 0, 0)
+    yello = color(255, 255, 0)
+    green = color(0, 255, 0)
+
+    ball1 = Ball(ball_pos, ball_r, red, ball_material)
+
+    image_plane = Plane(ball_pos, xMin, xMax, yMin, yMax, WIDTH, HEIGHT)
+    ray_tracing1 = RayTracer(camera_pos, ball1, image_plane)
+
+    IMG = image(WIDTH, HEIGHT)
+    for j in range(HEIGHT):
+        for i in range(WIDTH):
+            single_point = None
+            ray_tracing1.ray_direction(i, j)
+            ray_tracing1.sphere_to_ray()
+            ray_tracing1.ray_sphere_intersection()
+            ray_tracing1.hit_pos()
+            single_point1 = ray_tracing1.getHit()
+
+            if single_point1 is not None:
+                single_point = single_point1
+
+            if single_point is not None:
+                if single_point == single_point1:
+                    color_point = ball1.getColor(single_point, point_light.direction)
+            else:
+                color_point = color(0, 0, 0)
+            IMG.setColor(color_point, i, j)
+
+    IMG.saveImage("ball.ppm")
+
+if __name__ == '__main__':
+    main()
